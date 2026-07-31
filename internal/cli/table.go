50a2db5b67bf0bc235adf6a50eb94d0e217e1945
luem2 <lucianoepinol@gmail.com>
2026-07-30 21:10:21 -0300

    format & styling mini fix

--- internal/cli/table.go
+++ internal/cli/table.go
@@ -29,6 +29,7 @@ func printTable(writer io.Writer, rows [][]string) {
 		return
 	}
 	widths := tableWidths(rows)
+
 	for index, row := range rows {
 		printTableRow(writer, row, widths)
 		if index == 0 {
